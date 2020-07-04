a9b738f99415a2914f01f01cc79eca627b8f8dea
moroztaras <moroztaras1@gmail.com>
2020-07-04 15:58:09 +0300

    adding docker settining

---
+++ back/tests/Manual/MyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Manual;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class MyTest extends KernelTestCase
+{
+    public function testMyTest()
+    {
+        $a = [
+            'b' => 1,
+        ];
+
+        return;
+    }
+}
